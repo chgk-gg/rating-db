37a4e066418e2456b9d8f116210b9a3a077f816d
Jury Razumau <mail@razumau.net>
2025-08-05 21:41:37 +0200

    add option to download today’s backup

--- download.sh
+++ download.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# We create backups around 23:00 UTC, so a backup for today might not exist yet
-yesterday=$(date -v -1d '+%Y-%m-%d' 2>/dev/null || date -d "yesterday" '+%Y-%m-%d')
-url="https://pub-5200ce7fb4b64b5ea3b6b0b0f05cfcd5.r2.dev/${yesterday}_rating.backup"
+# By default, we download the backup for yesterday:
+# we create backups around 23:00 UTC, so a backup for today might not exist yet.
+if [ "$1" == "--today" ]; then
+  date_to_use=$(date '+%Y-%m-%d')
+else
+  date_to_use=$(date -v -1d '+%Y-%m-%d' 2>/dev/null || date -d "yesterday" '+%Y-%m-%d')
+fi
+
+url="https://pub-5200ce7fb4b64b5ea3b6b0b0f05cfcd5.r2.dev/${date_to_use}_rating.backup"
 curl -o rating.backup "$url"
